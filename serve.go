zenazn/goji

Better top-level AppEngine support

serve.go
@@ -1,0 +1,44 @@
+// +build !appengine
+
+package goji
+
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/zenazn/goji/bind"
+	"github.com/zenazn/goji/graceful"
+)
+
+func init() {
+	bind.WithFlag()
+	if fl := log.Flags(); fl&log.Ltime != 0 {
+		log.SetFlags(fl | log.Lmicroseconds)
+	}
+}
+
+// Serve starts Goji using reasonable defaults.
+func Serve() {
+	if !flag.Parsed() {
+		flag.Parse()
+	}
+
+	// Install our handler at the root of the standard net/http default mux.
+	// This allows packages like expvar to continue working as expected.
+	http.Handle("/", DefaultMux)
+
+	listener := bind.Default()
+	log.Println("Starting Goji on", listener.Addr())
+
+	graceful.HandleSignals()
+	bind.Ready()
+
+	err := graceful.Serve(listener, http.DefaultServeMux)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	graceful.Wait()
+}

serve_appengine.go
@@ -1,0 +1,13 @@
+// +build appengine
+
+package goji
+
+import "net/http"
+
+// Serve starts Goji using reasonable defaults.
+func Serve() {
+	// Install our handler at the root of the standard net/http default mux.
+	// This is required for App Engine, and also allows packages like expvar
+	// to continue working as expected.
+	http.Handle("/", DefaultMux)
+}

vanackere/slim

serve.go
@@ -1,0 +1,44 @@
+// +build !appengine
+
+package slim
+
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/vanackere/slim/bind"
+	"github.com/vanackere/slim/graceful"
+)
+
+func init() {
+	bind.WithFlag()
+	if fl := log.Flags(); fl&log.Ltime != 0 {
+		log.SetFlags(fl | log.Lmicroseconds)
+	}
+}
+
+// Serve starts Goji using reasonable defaults.
+func Serve() {
+	if !flag.Parsed() {
+		flag.Parse()
+	}
+
+	// Install our handler at the root of the standard net/http default mux.
+	// This allows packages like expvar to continue working as expected.
+	http.Handle("/", DefaultMux)
+
+	listener := bind.Default()
+	log.Println("Starting Goji on", listener.Addr())
+
+	graceful.HandleSignals()
+	bind.Ready()
+
+	err := graceful.Serve(listener, http.DefaultServeMux)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	graceful.Wait()
+}

serve_appengine.go
@@ -1,0 +1,13 @@
+// +build appengine
+
+package slim
+
+import "net/http"
+
+// Serve starts Goji using reasonable defaults.
+func Serve() {
+	// Install our handler at the root of the standard net/http default mux.
+	// This is required for App Engine, and also allows packages like expvar
+	// to continue working as expected.
+	http.Handle("/", DefaultMux)
+}